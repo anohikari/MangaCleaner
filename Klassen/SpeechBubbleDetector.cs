af5d39d3ecaff878019c1be590f9cc492bfd064e
anohikari <coreshine@protonmail.com>
2020-02-13 23:32:25 +0100

    V2. Initial Commit

--- Klassen/SpeechBubbleDetector.cs
+++ Klassen/SpeechBubbleDetector.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using Emgu.CV;
-using Emgu.CV.CvEnum;
-using Emgu.CV.Structure;
 
 namespace ImageHandler.Klassen
 {
@@ -12,7 +9,7 @@ namespace ImageHandler.Klassen
         public List<SpeechBubble> DetectSpeechBubbles(string ImageFile)
         {
             List<SpeechBubble> DetectedBubbles = new List<SpeechBubble>();
-            Mat img = CvInvoke.Imread("myimage.jpg", ImreadModes.AnyColor);
+            //Mat img = CvInvoke.Imread("myimage.jpg", ImreadModes.AnyColor);
             return DetectedBubbles;
         }
     }
